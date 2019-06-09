5ff9bee5064a34a4ee045421afd619fe85282d30
s15525 <s15525@pjwstk.edu.pl>
2019-06-09 15:19:41 +0200

    Add Kmeans logic with 2 method

--- src/Kmean.java
+++ src/Kmean.java
@@ -1,2 +1,60 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Kmean {
+    Map<String, List<List<Double>>> data;
+    double xCentroid;
+    double yCentroid;
+    double zCentroid;
+    Map<String, List<Double>> centroidMap = new HashMap<>();
+
+    Kmean(Map<String, List<List<Double>>> data) {
+        this.data = data;
+    }
+
+    public Map<String, List<Double>> centroid() {
+        for (List<List<Double>> row :
+                data.values()) {
+            List<Double> xyz = new ArrayList<>();
+            xCentroid = 0;
+            yCentroid = 0;
+            for (int i = 0; i < row.size(); i++) {
+                List<Double> onexyz = row.get(i);
+                xCentroid += onexyz.get(0);
+                yCentroid += onexyz.get(1);
+            }
+            xyz.add(xCentroid);
+            xyz.add(yCentroid);
+            centroidMap.put(getKey(data,row),xyz);
+        }
+        return centroidMap;
+    }
+
+    public void distanceToCentroid(Map<String, List<Double>> centroidMap ,List<Double> xyz){
+        double sum = 0;
+        double result = 0;
+        Map<String,Double> distanceMap = new HashMap<>();
+        for (List<Double> xyzCentroid:
+             centroidMap.values()) {
+            for (int i = 0; i < xyz.size(); i++) {
+                sum += Math.pow((xyzCentroid.get(i) - xyz.get(i)), 2);
+            }
+            result = Math.sqrt(sum);
+            distanceMap.put(getKey(centroidMap, xyzCentroid), result);
+        }
+
+    }
+
+    public <K, V> K getKey(Map<K, V> map, V value) {
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            if (value.equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        SimpleCSV input = new SimpleCSV("C:\\Users\\Lukasz Tolpa\\IdeaProjects\\Kmean\\Info\\dane_testowe.csv", true);
+        List<List<SimpleCSV.Element>> records = input.getRecords();
+        Map<String, List<List<Double>>> test = new HashMap<>();
+
+        for (List<SimpleCSV.Element> record :
+                records) {
+            List<Double> xyz = new ArrayList<>();
+
+            for (int i = 0; i < record.size(); i++) {
+                if (record.get(i).value instanceof Number) {
+                    xyz.add((Double) record.get(i).value);
+                } else if (!record.get(i).name.equals("nr")) {
+                    try {
+                        List<List<Double>> pom = test.get(record.get(i).value);
+                        pom.add(xyz);
+                        test.put((String) record.get(i).value, pom);
+                    } catch (NullPointerException a) {
+                        List<List<Double>> pom = new ArrayList<>();
+                        pom.add(xyz);
+                        test.put((String) record.get(i).value, pom);
+                    }
+                }
+            }
+        }
+
+        Kmean kmean = new Kmean(test);
+        for (List<List<Double>> row:
+             test.values()){
+            for (int i = 0 ; i < row.size() ; i++)
+            kmean.distanceToCentroid(kmean.centroid(),row.get(i));
+        }
+        System.out.println(test);
 
     }
 }
